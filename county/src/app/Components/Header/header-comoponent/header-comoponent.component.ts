221fcedfda96aba2f8eb3cab022e042df01016fc
danipier768 <danielmauricioperezrojas@gmail.com>
2024-07-02 17:01:58 -0500

    class add

--- county/src/app/Components/Header/header-comoponent/header-comoponent.component.ts
+++ county/src/app/Components/Header/header-comoponent/header-comoponent.component.ts
@@ -1,5 +1,6 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule} from '@angular/common';
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-header-comoponent',
@@ -8,6 +9,32 @@ import { Component } from '@angular/core';
   templateUrl: './header-comoponent.component.html',
   styleUrl: './header-comoponent.component.scss'
 })
-export class HeaderComoponentComponent {
+export class HeaderComoponentComponent implements OnInit {
+
+  showContent: boolean = true
+
+    textLabel = 'Ligth Mode'
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    
+  }
+  
+
+    onButtonClick(): void{
+
+      if (this.showContent ===  true) {
+        this.document.body.classList.add('my-body-class');
+        this.textLabel = 'Dark Mode'
+        this.showContent = false
+      }else{
+        this.document.body.classList.remove('my-body-class');
+        this.textLabel = 'Ligth Mode'
+        this.showContent = true
+      }
+      
+    }
+
+    ngOnInit(): void {
+     
+    }
 
 }
